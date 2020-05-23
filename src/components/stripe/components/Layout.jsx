e888364ae39b29e59f2cc1f0c9a1662a85dc304e
Walter <eschenbach.walter@gmail.com>
2020-05-22 20:12:52 -0500

    update env

--- src/components/stripe/components/Layout.jsx
+++ src/components/stripe/components/Layout.jsx
@@ -12,7 +12,7 @@ import { loadStripe } from "@stripe/stripe-js";
 // of fraud as customers browse your website.
 // Note: This is why we are adding it to a Layout component.
 
-const stripePromise = loadStripe(process.env.PUBLISHABLE_KEY);
+const stripePromise = loadStripe(process.env.REACT_APP_PUBLISHABLE_KEY);
 
 // TIP
 // call loadStripe outside of a component
@@ -20,7 +20,7 @@ const stripePromise = loadStripe(process.env.PUBLISHABLE_KEY);
 // called more times than it needs to
 
 const Layout = ({ children, title }) => {
-  console.log(process.env.PUBLISHABLE_KEY);
+  console.log(process.env);
   return (
     <>
       <GlobalStyles />
